1c0c7fe11a890903b959b3d7ae0eeabca3dc42a0
Vicky Guo <vicky.guo97@gmail.com>
2021-01-15 10:41:35 -0500

    update python cookbook include jupter notebook

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# pythoncookbook
+# Python Cookbook
 This is Python CookBook Code Repository
+
+
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/VickyGuo0907/python-cookbook)
--- chapter1/sample2.py
+++ chapter1/sample2.py
@@ -1,9 +1,4 @@
-
-# Unpacking Elements from Iterables of Arbitrary Length
-
-def drop_first_last(grades):
-  first, *middle, last = grades
-  return avg(middle)
+# 1.2  Unpacking Elements from Iterables of Arbitrary Length
 
 
 record = ('Dave', 'dave@example.com', '773-555-1212', '847-555-1212')
@@ -11,33 +6,34 @@ name, email, *phone_numbers = record
 
 print(name, email, phone_numbers)
 
-*trailing, current = [10,8,7,1,9,5,10,3]
-print(trailing)
-print(current)
+*trailing, current = [10, 8, 7, 1, 9, 5, 10, 3]
+print("trailing:", trailing)
+print("current:", current)
 
-records = [{'foo', 1, 2}, {'bar','hello'}, {'foo', 3, 4}]
+records = [{'foo', 1, 2}, {'bar', 'hello'}, {'foo', 3, 4}]
+
 
 def do_foo(x, y):
-  print('foo', x, y)
+    print('foo', x, y)
+
 
 def do_bar(s):
-  print('bar', s)
+    print('bar', s)
 
 
 for tag, *args in records:
-  if tag == 'foo':
-    do_foo(*args)
-  elif tag == 'bar':
-    do_bar(*args)
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'bar':
+        do_bar(*args)
 
-
-record = ('ACME', 50 , 123.45, (12, 18, 2012))
+record = ('ACME', 50, 123.45, (12, 18, 2012))
 
 name, *_, (*_, year) = record
 
 print(name, year)
 
-items = [1,10,7,4,5,9]
+items = [1, 10, 7, 4, 5, 9]
 head, *tail = items
 
 print(head)
